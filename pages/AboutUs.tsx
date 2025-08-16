3579e6585de380df413ce325c347ef4dfced6955
Nutthaweed <toto24.2546@gmail.com>
2025-08-17 01:18:42 +0700

    Update all code

---
+++ pages/AboutUs.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const AboutUs: React.FC = () => {
+    return(
+        <section className="flex flex-col min-h-screen  py-20 text-3xl md:text-4xl bg-[#344F71]">
+            <div className="container mx-auto px-11"> 
+                <p className="leading-tight max-w-5xl mx-auto text-4xl tracking-tight">
+                    We're come from <strong>Information of Technology and Digital Innovation(IBIT)</strong>
+                    We’re building more than a delivery app — we’re creating a trusted food ecosystem for students, employees, and local vendors. Born from real problems on campus, our mission is simple: make food access easier, safer, and fairer for everyone. This is for the people — not profit alone.
+                    That's why we made <strong>Saduak</strong>.
+                </p>
+            </div>
+            <div className="">
+
+            </div>
+        </section>
+    )
+}
+
+export default AboutUs
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Introduction from '../../pages/Introduction';
+import AboutUs from '../../pages/AboutUs';
 
 
 const Home: React.FC = () => {
   return (
+    <div>
    <Introduction/>
+   <AboutUs />
+   </div>
   );
 };
 
